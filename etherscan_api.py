f359020a3a3ac975a4f58223d1d4cef600f0e3e0
sunhaobo1k <125957214+sunhaobo1k@users.noreply.github.com>
2023-06-06 02:03:54 +0800

    Update etherscan_api.py
    1.增添了BSCScan Gasprice的獲取
    2.增添了PolygonScan Gasprice的獲取

--- etherscan_api.py
+++ etherscan_api.py
@@ -1,7 +1,9 @@
 import requests
 import time
 
-api_key = 'YOUR API KEY HERE'
+eth_api_key = 'YOUR ETHScan API KEY HERE'
+bsc_api_key = 'YOUR BSCScan API KEY HERE'
+polygon_api_key = 'YOUR PolygonScan API KEY HERE'
 
 class Ethereum:
     def get_gas_price(self):
@@ -10,7 +12,71 @@ class Ethereum:
         payload = {
             "module": "gastracker",
             "action": "gasoracle",
-            "apikey": api_key
+            "apikey": eth_api_key
+        }
+
+        while True:
+            try:
+                response = requests.get(url, params=payload, timeout=5)  # 设置超时时间为5秒
+                response.raise_for_status()  # 检查请求是否成功
+                data = response.json()
+
+                if data["status"] == "1":
+                    print("Safe gas price: {} Gwei".format(data["result"]["SafeGasPrice"]))
+                    print("Propose gas price: {} Gwei".format(data["result"]["ProposeGasPrice"]))
+                    print("Fast gas price: {} Gwei".format(data["result"]["FastGasPrice"]))
+                    return float(data["result"]["ProposeGasPrice"])
+                else:
+                    print("Error occurred: {}".format(data["message"]))
+
+            except (requests.RequestException, ValueError) as e:
+                print("Error occurred during gas price retrieval:", str(e))
+
+            print("Retrying after 5 seconds...")
+            time.sleep(5)
+
+        return None  # 返回 None 表示未能成功获取 gas price
+    
+class BSC:
+    def get_gas_price(self):
+        url = "https://api.bscscan.com/api"
+
+        payload = {
+            "module": "gastracker",
+            "action": "gasoracle",
+            "apikey": bsc_api_key
+        }
+
+        while True:
+            try:
+                response = requests.get(url, params=payload, timeout=5)  # 设置超时时间为5秒
+                response.raise_for_status()  # 检查请求是否成功
+                data = response.json()
+
+                if data["status"] == "1":
+                    print("Safe gas price: {} Gwei".format(data["result"]["SafeGasPrice"]))
+                    print("Propose gas price: {} Gwei".format(data["result"]["ProposeGasPrice"]))
+                    print("Fast gas price: {} Gwei".format(data["result"]["FastGasPrice"]))
+                    return float(data["result"]["ProposeGasPrice"])
+                else:
+                    print("Error occurred: {}".format(data["message"]))
+
+            except (requests.RequestException, ValueError) as e:
+                print("Error occurred during gas price retrieval:", str(e))
+
+            print("Retrying after 5 seconds...")
+            time.sleep(5)
+
+        return None  # 返回 None 表示未能成功获取 gas price
+    
+class Polygon:
+    def get_gas_price(self):
+        url = "https://api.polygonscan.com/api"
+
+        payload = {
+            "module": "gastracker",
+            "action": "gasoracle",
+            "apikey": polygon_api_key
         }
 
         while True:
